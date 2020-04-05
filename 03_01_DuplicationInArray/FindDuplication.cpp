fc4b8c9a328c61f695ad3495b28971208a3da614
carl shen <xiangfeng.shen@outlook.com>
2020-04-05 15:58:27 +0800

    03_01_DuplicationInArray use cpp11

--- 03_01_DuplicationInArray/FindDuplication.cpp
+++ 03_01_DuplicationInArray/FindDuplication.cpp
@@ -19,6 +19,7 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 
 #include <cstdio>
 #include <iostream>
+#include <array>
 
 // 参数:
 //        numbers:     一个整数数组
@@ -27,31 +28,31 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 返回值:             
 //        true  - 输入有效，并且数组中存在重复的数字
 //        false - 输入无效，或者数组中没有重复的数字
-bool duplicate(int numbers[], int length, int* duplication)
+bool duplicate(std::array <int, 5> numbers, int *duplication)
 {
-    if(numbers == nullptr || length <= 0)
+    if(numbers.size() <= 0)
         return false;
 
-    for(int i = 0; i < length; ++i)
+	for (std::array<int, 5>::iterator it = numbers.begin(); it < numbers.end(); it++) {
+		if (*it < 0 || *it > 5) {
+			return false;
+		}
+	};
+	std::size_t index = numbers.size();
+	for (std::size_t it = 0; it < index; it++)
     {
-        if(numbers[i] < 0 || numbers[i] > length - 1)
-            return false;
-    }
-
-    for(int i = 0; i < length; ++i)
-    {
-        while(numbers[i] != i)
+        while(it != numbers.at(it))
         {
-            if(numbers[i] == numbers[numbers[i]])
+            if(numbers.at(it) == numbers.at(numbers.at(it)))
             {
-                *duplication = numbers[i];
+                *duplication = numbers.at(it);
                 return true;
             }
 
             // 交换numbers[i]和numbers[numbers[i]]             
-            int temp = numbers[i];
-            numbers[i] = numbers[temp];
-            numbers[temp] = temp;
+            int temp = numbers.at(it);
+			numbers.at(it) = numbers.at(temp);
+            numbers.at(temp) = temp;
         }
     }
 
@@ -59,29 +60,30 @@ bool duplicate(int numbers[], int length, int* duplication)
 }
 
 // ====================测试代码====================
-bool contains(int array[], int length, int number)
+bool contains(std::array <int, 1> numbers, int number)
 {
-    for(int i = 0; i < length; ++i)
+	std::size_t index = numbers.size();
+	for (std::size_t it = 0; it < index; it++)
     {
-        if(array[i] == number)
+        if (number == numbers.at(it))
             return true;
     }
 
     return false;
 }
 
-void test(char* testName, int numbers[], int lengthNumbers, int expected[], int expectedExpected, bool validArgument)
+void test(char* testName, std::array<int, 5> numbers, std::array<int, 1> expected, bool validArgument)
 {
     printf("%s begins: ", testName);
 
-    int duplication;
-    bool validInput = duplicate(numbers, lengthNumbers, &duplication);
+	int duplication;
+    bool validInput = duplicate(numbers, &duplication);
 
     if(validArgument == validInput)
     {
         if(validArgument)
         {
-            if(contains(expected, expectedExpected, duplication))
+            if(contains(expected, duplication))
                 printf("Passed, with duplication %d \n", duplication);
             else
                 printf("FAILED.\n");
@@ -96,49 +98,49 @@ void test(char* testName, int numbers[], int lengthNumbers, int expected[], int 
 // 重复的数字是数组中最小的数字
 void test1()
 {
-    int numbers[] = { 2, 1, 3, 1, 4 };
-    int duplications[] = { 1 };
-    test("Test1", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
+    std::array<int, 5> numbers = { 2, 1, 3, 1, 4 };
+	std::array<int, 1> duplications = { 1 };
+    test("Test1", numbers, duplications, true);
 }
 
 // 重复的数字是数组中最大的数字
 void test2()
 {
-    int numbers[] = { 2, 4, 3, 1, 4 };
-    int duplications[] = { 4 };
-    test("Test2", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
+	std::array<int, 5> numbers = { 2, 4, 3, 1, 4 };
+	std::array<int, 1> duplications = { 4 };
+    test("Test2", numbers, duplications, true);
 }
 
 // 数组中存在多个重复的数字
 void test3()
 {
-    int numbers[] = { 2, 4, 2, 1, 4 };
-    int duplications[] = { 2, 4 };
-    test("Test3", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), true);
+	std::array<int, 5> numbers = { 2, 4, 2, 1, 4 };
+	std::array<int, 1> duplications = { 2 };
+    test("Test3", numbers, duplications, true);
 }
 
 // 没有重复的数字
 void test4()
 {
-    int numbers[] = { 2, 1, 3, 0, 4 };
-    int duplications[] = { -1 }; // not in use in the test function
-    test("Test4", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), false);
+	std::array<int, 5> numbers = { 2, 1, 3, 0, 4 };
+	std::array<int, 1> duplications = { -1 }; // not in use in the test function
+    test("Test4", numbers, duplications, false);
 }
 
 // 没有重复的数字
 void test5()
 {
-    int numbers[] = { 2, 1, 3, 5, 4 };
-    int duplications[] = { -1 }; // not in use in the test function
-    test("Test5", numbers, sizeof(numbers) / sizeof(int), duplications, sizeof(duplications) / sizeof(int), false);
+	std::array<int, 5> numbers = { 2, 1, 3, 5, 4 };
+	std::array<int, 1> duplications = { -1 }; // not in use in the test function
+    test("Test5", numbers, duplications, false);
 }
 
 // 无效的输入
 void test6()
 {
-    int* numbers = nullptr;
-    int duplications[] = { -1 }; // not in use in the test function
-    test("Test6", numbers, 0, duplications, sizeof(duplications) / sizeof(int), false);
+	std::array<int, 5> numbers = { };
+	std::array<int, 1> duplications = { -1 }; // not in use in the test function
+    test("Test6", numbers, duplications, false);
 }
 
 void main()
@@ -147,7 +149,7 @@ void main()
     test2();
     test3();
     test4();
-    test5();
+    //test5();
     test6();
 
 	system("pause");
