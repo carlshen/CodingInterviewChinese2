cfd19505ffa2db3436c46a797893536f40d40773
carl shen <xiangfeng.shen@outlook.com>
2020-04-05 17:49:19 +0800

    05_ReplaceSpaces update with cpp11 string

--- 05_ReplaceSpaces/ReplaceSpaces.cpp
+++ 05_ReplaceSpaces/ReplaceSpaces.cpp
@@ -19,64 +19,69 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+using std::string;
 
 /*length 为字符数组str的总容量，大于或等于字符串str的实际长度*/
-void ReplaceBlank(char str[], int length)
+std::string ReplaceBlank(std::string str)
 {
-    if(str == nullptr && length <= 0)
-        return;
+    if(str.length() <= 0)
+        return str;
 
     /*originalLength 为字符串str的实际长度*/
-    int originalLength = 0;
+    int originalLength = str.length();
     int numberOfBlank = 0;
-    int i = 0;
-    while(str[i] != '\0')
+	for (std::string::const_iterator it = str.begin(); it != str.end(); it++)
     {
-        ++ originalLength;
-
-        if(str[i] == ' ')
+        if(*it == ' ')
             ++ numberOfBlank;
-
-        ++ i;
     }
+	//std::cout << "ReplaceBlank blank size: " << numberOfBlank << std::endl;
 
     /*newLength 为把空格替换成'%20'之后的长度*/
-    int newLength = originalLength + numberOfBlank * 2;
-    if(newLength > length)
-        return;
+    size_t newLength = originalLength + numberOfBlank * 2;
+	string result(str);
+	result.resize(newLength);
 
-    int indexOfOriginal = originalLength;
-    int indexOfNew = newLength;
-    while(indexOfOriginal >= 0 && indexOfNew > indexOfOriginal)
+	size_t indexOfOriginal = originalLength;
+	size_t indexOfNew = newLength;
+	//std::cout << "ReplaceBlank originalLength size: " << originalLength << std::endl;
+	//std::cout << "ReplaceBlank newLength size: " << newLength << std::endl;
+	while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal)
     {
         if(str[indexOfOriginal] == ' ')
         {
-            str[indexOfNew --] = '0';
-            str[indexOfNew --] = '2';
-            str[indexOfNew --] = '%';
+			result[indexOfNew --] = '0';
+			result[indexOfNew --] = '2';
+			result[indexOfNew --] = '%';
         }
         else
         {
-            str[indexOfNew --] = str[indexOfOriginal];
+			result[indexOfNew --] = str[indexOfOriginal];
         }
 
         -- indexOfOriginal;
     }
+	return result;
 }
 
 // ====================测试代码====================
-void Test(char* testName, char str[], int length, char expected[])
+void Test(char* testName, std::string str, std::string expected)
 {
     if(testName != nullptr)
         printf("%s begins: ", testName);
+	if (expected.empty() && str.empty()) {
+		printf("passed.\n");
+		return;
+	}
 
-    ReplaceBlank(str, length);
+	std::string result = ReplaceBlank(str);
+	std::cout << "ReplaceBlank result: " << result.c_str() << std::endl;
 
-    if(expected == nullptr && str == nullptr)
-        printf("passed.\n");
-    else if(expected == nullptr && str != nullptr)
+    if(!expected.empty() && result.empty())
         printf("failed.\n");
-    else if(strcmp(str, expected) == 0)
+    else if(expected.empty() && !result.empty())
+        printf("failed.\n");
+    else if(result == expected)
         printf("passed.\n");
     else
         printf("failed.\n");
@@ -85,79 +90,63 @@ void Test(char* testName, char str[], int length, char expected[])
 // 空格在句子中间
 void Test1()
 {
-    const int length = 100;
-
-    char str[length] = "hello world";
-    Test("Test1", str, length, "hello%20world");
+	std::string str("hello world");
+    Test("Test1", str, "hello%20world");
 }
 
 // 空格在句子开头
 void Test2()
 {
-    const int length = 100;
-
-    char str[length] = " helloworld";
-    Test("Test2", str, length, "%20helloworld");
+	std::string  str = " helloworld";
+    Test("Test2", str, "%20helloworld");
 }
 
 // 空格在句子末尾
 void Test3()
 {
-    const int length = 100;
-
-    char str[length] = "helloworld ";
-    Test("Test3", str, length, "helloworld%20");
+	std::string  str = "helloworld ";
+    Test("Test3", str, "helloworld%20");
 }
 
 // 连续有两个空格
 void Test4()
 {
-    const int length = 100;
-
-    char str[length] = "hello  world";
-    Test("Test4", str, length, "hello%20%20world");
+	std::string  str = "hello  world";
+    Test("Test4", str, "hello%20%20world");
 }
 
 // 传入nullptr
 void Test5()
 {
-    Test("Test5", nullptr, 0, nullptr);
+	Test("Test5", nullptr, nullptr);
 }
 
 // 传入内容为空的字符串
 void Test6()
 {
-    const int length = 100;
-
-    char str[length] = "";
-    Test("Test6", str, length, "");
+	std::string  str = "";
+    Test("Test6", str, "");
 }
 
 //传入内容为一个空格的字符串
 void Test7()
 {
-    const int length = 100;
-
-    char str[length] = " ";
-    Test("Test7", str, length, "%20");
+	std::string str = " ";
+    Test("Test7", str, "%20");
 }
 
 // 传入的字符串没有空格
 void Test8()
 {
-    const int length = 100;
-
-    char str[length] = "helloworld";
-    Test("Test8", str, length, "helloworld");
+	std::string str = "helloworld";
+    Test("Test8", str, "helloworld");
 }
 
 // 传入的字符串全是空格
 void Test9()
 {
-    const int length = 100;
-
-    char str[length] = "   ";
-    Test("Test9", str, length, "%20%20%20");
+	std::string  str = "   ";
+    Test("Test9", str, "%20%20%20");
 }
 
 int main(int argc, char* argv[])
@@ -166,7 +155,7 @@ int main(int argc, char* argv[])
     Test2();
     Test3();
     Test4();
-    Test5();
+	//Test5();
     Test6();
     Test7();
     Test8();
