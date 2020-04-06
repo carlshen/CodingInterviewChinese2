f840304589be3b5e05927e2d0495da18f40f6981
carl shen <xiangfeng.shen@outlook.com>
2020-04-06 22:15:55 +0800

    update 58_01_ReverseWordsInSentence to cpp11

--- 58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
+++ 58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
@@ -19,38 +19,58 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 
 #include <cstdio>
 #include <iostream>
-#include "..\Utilities\StringUtil.h"
 #include <string>
 
-char* ReverseSentence(char *pData)
+void Reverse(std::string pData, std::string::iterator pBegin, std::string::iterator pEnd)
 {
-    if(pData == nullptr)
-        return nullptr;
+	if (pData.empty())
+		return;
+	if (pBegin >= pEnd)
+		return;
 
-    char *pBegin = pData;
+	while (pBegin < pEnd)
+	{
+		char temp = *pBegin;
+		*pBegin = *pEnd;
+		*pEnd = temp;
 
-    char *pEnd = pData;
-    while(*pEnd != '\0')
-        pEnd ++;
-    pEnd--;
+		pBegin++, pEnd--;
+	}
+}
 
-    // 翻转整个句子
-    Reverse(pBegin, pEnd);
+std::string ReverseSentence(std::string str)
+{
+    if(str.empty())
+        return str;
+
+	std::string pData(str);
+	// 翻转整个句子
+	Reverse(pData, pData.begin(), --pData.end());
+	//std::cout << "ReverseSentence pData: " << pData.c_str() << std::endl;
 
     // 翻转句子中的每个单词
-    pBegin = pEnd = pData;
-    while(*pBegin != '\0')
+	std::string::iterator pBegin = pData.begin();
+	std::string::iterator pEnd = pData.begin();
+	std::string::iterator pStop = pData.end();
+    while(pBegin != pStop)
     {
-        if(*pBegin == ' ')
+		if(*pBegin == ' ')
         {
             pBegin ++;
             pEnd ++;
         }
-        else if(*pEnd == ' ' || *pEnd == '\0')
+        else if(*pEnd == ' ')
         {
-            Reverse(pBegin, --pEnd);
+            Reverse(pData, pBegin, --pEnd);
+			//std::cout << "Reverse pData: " << pData.c_str() << std::endl;
             pBegin = ++pEnd;
         }
+		else if (pEnd == (pStop-1))
+		{
+			Reverse(pData, pBegin, pEnd);
+			//std::cout << "end pData: " << pData.c_str() << std::endl;
+			pBegin = ++pEnd;
+		}
         else
             pEnd ++;
     }
@@ -59,18 +79,22 @@ char* ReverseSentence(char *pData)
 }
 
 // ====================测试代码====================
-void Test(const char* testName, char* input, const char* expectedResult)
+void Test(const char* testName, std::string input, std::string expectedResult)
 {
     if(testName != nullptr)
-        printf("%s begins: ", testName);
+        printf("%s begins: \n", testName);
 
-	printf("\nbefore input: %s \n ", input);
-	printf("before expectedResult: %s \n ", expectedResult);
-    ReverseSentence(input);
-	printf("after reverse input: %s \n ", input);
+	/*if ((input.empty() && expectedResult.empty())) {
+		printf("Passed.\n\n");
+		return;
+	}*/
+	std::cout << "before input: " << input.c_str() << std::endl;
+	std::cout << "before expectedResult: " << expectedResult.c_str() << std::endl;
+	std::string result = ReverseSentence(input);
+	std::cout << "after reverse input: " << result.c_str() << std::endl;
 
-    if((input == nullptr && expectedResult == nullptr)
-        || (input != nullptr && strcmp(input, expectedResult) == 0))
+    if((input.empty() && expectedResult.empty())
+        || (!input.empty() && (result == expectedResult)))
         printf("Passed.\n\n");
     else
         printf("Failed.\n\n");
@@ -79,8 +103,8 @@ void Test(const char* testName, char* input, const char* expectedResult)
 // 功能测试，句子中有多个单词
 void Test1()
 {
-    char input[] = "I am a student.";
-    char expected[] = "student. a am I";
+	std::string input = "I am a student.";
+	std::string expected = "student. a am I";
 
     Test("Test1", input, expected);
 }
@@ -88,8 +112,8 @@ void Test1()
 // 功能测试，句子中只有一个单词
 void Test2()
 {
-    char input[] = "Wonderful";
-    char expected[] = "Wonderful";
+	std::string input = "Wonderful";
+	std::string expected = "Wonderful";
 
     Test("Test2", input, expected);
 }
@@ -97,7 +121,9 @@ void Test2()
 // 鲁棒性测试
 void Test3()
 {
-    Test("Test3", nullptr, nullptr);
+	std::string input = "We are students.";
+	std::string expected = "students. are We";
+    Test("Test3", input, expected);
 }
 
 // 边界值测试，测试空字符串
@@ -109,8 +135,8 @@ void Test4()
 // 边界值测试，字符串中只有空格
 void Test5()
 {
-    char input[] = "   ";
-    char expected[] = "   ";
+	std::string input = "   ";
+	std::string expected = "   ";
     Test("Test5", input, expected);
 }
 
